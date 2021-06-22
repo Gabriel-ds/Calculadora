de799190972d0f1cb2d996622bf45ce043db4b23
Gabriel dos Santos <gabrields1910@gmail.com>
2021-06-22 07:32:55 -0300

    inicio javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+//Selecionando elementos HTML
+var resp = document.querySelector('.resposta')
+var zero = document.querySelector('#zero')
+
+resp.innerHTML = zero.value                //colocar uma funcção onclick em cada botão 
